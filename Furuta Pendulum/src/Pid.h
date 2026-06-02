574a28c5d8b61c0f7c1d39e8e620c6f5e262330b
sohamdave95 <davesoh929@gmail.com>
2026-06-01 20:34:05 -0400

    Updated to match style guide

--- Furuta Pendulum/src/Pid.h
+++ Furuta Pendulum/src/Pid.h
@@ -4,9 +4,6 @@
 // Date: April 12, 2026
 
 
-
-#define PID_H
-
 #include <Arduino.h>
 
 class PID { // setting private vars within the class
@@ -14,68 +11,50 @@ class PID { // setting private vars within the class
         float kp;
         float ki;
         float kd;
-
         float integral;
         float last_error;
         float last_time;
-
         float maxAngle;
         float minAngle;
-    
     public:
         PID(float p, float i, float d, float mxAngle, float mnAngle){ // public function to create an instance of that class
             kp = p;
             ki = i;
             kd = d;
-
             integral = 0.0f;
             last_error = 0.0f;
             last_time = 0.0f;
-
             maxAngle = mxAngle;
             minAngle = mnAngle;
-
         }
 
         float calculation(float target, float current_deg, float current_time, bool modulus){ // method within it, does the PID Math
             if (last_time == 0.0f){
                 last_time = current_time;
             }
-
             float dt = current_time - last_time;
             float error;
-
             if (modulus) {
-                
-                error = fmodf((target - current_deg + 180.0f), 360.0f);
-                if (error < 0) error += 360.0f; 
-                error -= 180.0f;
+                WrapError(error, target, current_deg);
             }
             else {
                 error = target - current_deg;
             }
-
             float derivative = 0.0f;
             if (dt > 0.0f){
                 derivative = (error - last_error)/dt;
             }
-
-
-
             integral += error*dt;
-
-
-            
-
             float output = (kp*error) + (ki*integral) + (kd*derivative);
-
             last_error = error;
             last_time = current_time;
-
             return output;
+        }
 
-
-
+        void WrapError(float &pendError, float pendTarget, float pendCurrentDeg){
+            pendError = fmodf((pendTarget - pendCurrentDeg + 180.0f), 360.0f);
+            if (pendError < 0) pendError += 360.0f; 
+            pendError -= 180.0f;
         }
 
 };
--- Furuta Pendulum/src/main.cpp
+++ Furuta Pendulum/src/main.cpp
@@ -1,32 +1,24 @@
-#include <Arduino.h>
 #include "AS5600.h"
 #include <ESP32Encoder.h>
 #include <Pid.h>
 
-
-#define CLK 25 // CLK ENCODER 
-#define DT 26 // DT ENCODER 
-
-
 AS5600 as5600; // using those libararies to set up instances for my encoders
-
 ESP32Encoder n20;
-
-int signalA = 18; // DC motors have 2 polarities. These basically just refer to what direction to spin
-int signalB = 19;
-float pendOffset = 74;
-
-int deadzone = 55; // my dc motor doesn't move until a minimum of 55 is applied on the analogWrite
-// basically so our motor is always moving, to make microadjustments
-
-
-// in ORDER: P, I, D, Max angle, min angle
 PID arm_pid(0.5f, 0.0f, 0.11f, 340.0f, -340.0f); // setting up class instances with these parameters
 PID pend_pid(10.0f, 0.0f, 0.1f, 208.0f, 168.0f);
+
+const int signalA = 18; // DC motors have 2 polarities. These basically just refer to what direction to spin
+const int signalB = 19;
+const float pendOffset = 74;
+const int clock =  25 // CLK ENCODER 
+const int data  = 26 // DT ENCODER 
+const int deadzone = 55; // my dc motor doesn't move until a minimum of 55 is applied on the analogWrite
+const int clock =  25 // CLK ENCODER 
+const int data  = 26 // DT ENCODER
+
 unsigned long lastLoopTime = 0; // using a variable here to keep the poll rate (hertz), always constant to keep dt stable
 
 void setup() {
-    
 // basically just setting up some basics for the libararies, also setting the i2c speed for the encoders, plus serial speed 
   n20.attachHalfQuad ( DT, CLK );
   n20.setCount (0);
@@ -41,40 +33,39 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
-if (micros() - lastLoopTime >= 1000) {
-  lastLoopTime = micros(); // regulates our loop rate, better than using delay
+  if (micros() - lastLoopTime >= 1000){
+    lastLoopTime = micros(); // regulates our loop rate, better than using delay
 
-  float current_time = micros() / 1000000.0f; // finds current time (VERY important)
+    float current_time = micros() / 1000000.0f; // finds current time (VERY important)
 
-  // getting raw sensor data and stuff, also making a variable linked to that class instance with the method in it
-  float rawdeg = (as5600.readAngle() * AS5600_RAW_TO_DEGREES);
-  float current_pend_deg = fmodf(rawdeg + pendOffset, 360.0f);
-  float current_arm_deg = n20.getCount() / 2.0f;
-  float arm_output = arm_pid.calculation(0, current_arm_deg, current_time, false);
-  float pend_output = pend_pid.calculation(185, current_pend_deg, current_time, true);
+    // getting raw sensor data and stuff, also making a variable linked to that class instance with the method in it
+    float rawdeg = (as5600.readAngle() * AS5600_RAW_TO_DEGREES);
+    float current_pend_deg = fmodf(rawdeg + pendOffset, 360.0f);
+    float current_arm_deg = n20.getCount() / 2.0f;
+    float arm_output = arm_pid.calculation(0, current_arm_deg, current_time, false);
+    float pend_output = pend_pid.calculation(185, current_pend_deg, current_time, true);
 
-  float totalOutput = -arm_output + pend_output;
-// total output that goes to the motor, arm output is negative here since these 2 technically counteract each other
+    float totalOutput = -arm_output + pend_output;
+  // total output that goes to the motor, arm output is negative here since these 2 technically counteract each other
 
-  bool safeRange = ((current_pend_deg > 165 && current_pend_deg < 205) && ((current_arm_deg < 340) && current_arm_deg > -340));
-  // only writes to the motor when the sensor values are within these parameters, its a hardcoded safety to prevent snapping wires, etc
-  if (safeRange){
+    bool safeRange = ((current_pend_deg > 165 && current_pend_deg < 205) && ((current_arm_deg < 340) && current_arm_deg > -340));
+    // only writes to the motor when the sensor values are within these parameters, its a hardcoded safety to prevent snapping wires, etc
+    if (safeRange){
 
-  if (totalOutput > 0){
-    // writes in the appropriate direction
-    analogWrite(signalA, constrain(totalOutput + deadzone, 0, 255));
-    analogWrite(signalB, 0);
-  }else{
-    analogWrite(signalA, 0);
-    analogWrite(signalB, constrain(fabsf(totalOutput - deadzone), 0, 255));
-    // fabsf (absolute val) from arduino libarary to use absolute value, (you cant write a negative pwm val to a motor)
+      if (totalOutput > 0){
+        // writes in the appropriate direction
+        analogWrite(signalA, constrain(totalOutput + deadzone, 0, 255));
+        analogWrite(signalB, 0);
+      }
+      else{
+        analogWrite(signalA, 0);
+        analogWrite(signalB, constrain(fabsf(totalOutput - deadzone), 0, 255));
+        // fabsf (absolute val) from arduino libarary to use absolute value, (you cant write a negative pwm val to a motor)
+      }
+    }
+    else{
+      totalOutput = 0; // if not, sets the output to 0 for safety, the motor doesnt move.
+
+    }
   }
-}
-  else{
-    totalOutput = 0; // if not, sets the output to 0 for safety, the motor doesnt move.
-
-  }
-  
-
-}
 }
